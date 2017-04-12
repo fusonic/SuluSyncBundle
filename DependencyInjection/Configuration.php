2de23e0cd47ea43a8630a675ebe61f53d0f8efba
Andreas Fröwis <andreas.froewis@fusonic.net>
2017-04-12 10:11:38 +0200

    Double quotes

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -13,7 +13,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('fusonic_sulu_sync');
+        $rootNode = $treeBuilder->root("fusonic_sulu_sync");
         return $treeBuilder;
     }
 }
